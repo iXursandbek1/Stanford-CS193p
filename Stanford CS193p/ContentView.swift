c5100efae7ece778381bf5546f1407ecb8bde65a
Xursandbek <xursandbek06082001@mail.com>
2024-10-12 14:16:18 +0500

    Initial Commit

---
+++ Stanford CS193p/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Stanford CS193p
+//
+//  Created by Xursandbek Qambaraliyev on 12/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Stanford CS193p/Stanford_CS193pApp.swift
@@ -0,0 +1,17 @@
+//
+//  Stanford_CS193pApp.swift
+//  Stanford CS193p
+//
+//  Created by Xursandbek Qambaraliyev on 12/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Stanford_CS193pApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
